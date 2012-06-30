263638882e47e670768092dbdd613772234a4abc
raimon <raimon49@hotmail.com>
2012-06-30 12:55:33 +0900

    install helper script.

--- install_cui.sh
+++ install_cui.sh
@@ -5,6 +5,15 @@
 mkdir -p ~/local/bin
 mkdir -p ~/.zsh/cache
 
+# for Git on Bash
+if [ -x "`which wget 2> /dev/null`" ]; then
+    wget 'https://github.com/git/git/raw/master/contrib/completion/git-completion.bash' --no-check-certificate -O ~/local/bin/git-completion.bash
+    wget 'https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh' --no-check-certificate -O ~/local/bin/git-prompt.sh
+elif [ -x "`which curl 2> /dev/null`" ]; then
+    curl 'https://github.com/git/git/raw/master/contrib/completion/git-completion.bash' -L -o ~/local/bin/git-completion.bash
+    curl 'https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh' -L -o ~/local/bin/git-prompt.sh
+fi
+
 ln -s ~/works/git/dotfiles/.gitconfig       ~
 ln -s ~/works/git/dotfiles/.hgrc            ~
 ln -s ~/works/git/dotfiles/.hgeditor        ~
