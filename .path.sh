93ae189ff934ffa79a8447db6b962717786fa6a2
raimon <raimon49@hotmail.com>
2012-08-02 10:55:00 +0900

    fix manpath

--- .path.sh
+++ .path.sh
@@ -1,7 +1,7 @@
 ## .path.sh
 
 export PATH="${HOME}/local/bin:${PATH}"
-export MANPATH="${HOME}/local/man:${MANPATH}"
+export MANPATH="${HOME}/local/share/man:${MANPATH}"
 
 # for VVM
 test -f ~/.vvm/etc/login && source ~/.vvm/etc/login
