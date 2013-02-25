62f85dd1342ede8f70da7bb11dc83969a842b345
raimon <raimon49@hotmail.com>
2013-02-25 19:30:46 +0900

    Fix spell

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ fi
 [ ! -d ~/works/git ] && mkdir -p ~/works/git
 cd ~/works/git && git clone --recursive git://github.com/raimon49/dotfiles.git && cd dotfiles && yes | ./install_cui.sh
 
-echo "bootstraped."
+echo "\nbootstrapped."
 exit 0
