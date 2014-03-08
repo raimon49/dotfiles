29a55907a6e97dd7c0388174a6843a608728261a
raimon <raimon49@hotmail.com>
2014-03-08 11:51:43 +0900

    Add nodebrew

--- .path.sh
+++ .path.sh
@@ -12,3 +12,6 @@ export MANPATH="${HOME}/local/share/man:${MANPATH}"
 
 # for VVM
 test -f ~/.vvm/etc/login && source ~/.vvm/etc/login
+
+# for Node
+test -f ~/.nodebrew/nodebrew && export PATH="${HOME}/.nodebrew/current/bin:${PATH}"
