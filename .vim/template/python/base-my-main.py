b31019d46409a489b4098701111000a679c9c957
raimon <raimon49@hotmail.com>
2015-08-01 22:33:38 +0900

    Update my Python script template as version 2.7 or later

--- .vim/template/python/base-my-main.py
+++ .vim/template/python/base-my-main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='script description')
+    parser.add_argument('-n', '--dryrun',
+                        action='store_true',
+                        default=False,
+                        help='dry run this script')
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
+    if args.dryrun:
+        sys.exit(0)
+
     {{_cursor_}}
+
 
 if __name__ == '__main__':
     main()
