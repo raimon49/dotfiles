bd2f367eccf4512bd7f4d565a10e60cd29ecaca0
raimon <raimon49@hotmail.com>
2015-12-07 08:40:58 +0900

    [WIP] Create script skeleton #2

---
+++ install_version_manager.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# nodebrew
+curl -L git.io/nodebrew | perl - setup
+
+# pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+
+# TODO: rbenv
