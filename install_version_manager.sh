f9ba925612b902ed54acb9101fa9df3673427d37
raimon <raimon49@hotmail.com>
2015-12-12 21:44:15 +0900

    Install rbenv and ruby-build #2

--- install_version_manager.sh
+++ install_version_manager.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # nodebrew
-curl -L git.io/nodebrew | perl - setup
+[ ! -d ~/.nodebrew ] && \
+    curl -L git.io/nodebrew | perl - setup
 
 # pyenv
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+[ ! -d ~/.pyenv ] && \
+    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
-# TODO: rbenv
+# rbenv
+[ ! -d ~/.rbenv ] && \
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv && \
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
