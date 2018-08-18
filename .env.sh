2b61e7785a3f4fc9d8b3ad60d08b5e4b1de2e36b
raimon <raimon49@hotmail.com>
2018-08-18 11:49:43 +0900

    Add quote at environment variables

--- .env.sh
+++ .env.sh
@@ -1,24 +1,24 @@
 ## .env.sh
 
-export LANG=ja_JP.UTF-8
-export LC_CTYPE=ja_JP.UTF-8
-export EDITOR=vim
-export SVN_EDITOR=vim
+export LANG="ja_JP.UTF-8"
+export LC_CTYPE="ja_JP.UTF-8"
+export EDITOR="vim"
+export SVN_EDITOR="vim"
 export SCREENDIR="${HOME}/.screen"
-export LSCOLORS=gxFxCxdxBxegedabagacad
+export LSCOLORS="gxFxCxdxBxegedabagacad"
 export LS_COLORS='di=01;36:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 if [ -x "`which lv 2> /dev/null`" ]; then
-    export PAGER=lv
+    export PAGER="lv"
     export LV="-Ou8 -c -Sh1;36 -Su1;4;32 -Ss7;37;1;33"
 elif [ -x "`which less 2> /dev/null`" ]; then
-    export PAGER=less
+    export PAGER="less"
     export LESS="-R"
-    export LESSCHARSET=utf-8
+    export LESSCHARSET="utf-8"
     alias lv="less"
 fi
 
 # See also: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
-test -x /usr/local/bin/brew && export HOMEBREW_NO_ANALYTICS=1
+test -x /usr/local/bin/brew && export HOMEBREW_NO_ANALYTICS="1"
 
 man() {
     env \
