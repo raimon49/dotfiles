84692fa2d685c196118144a7a4402b7b5c1c3b62
raimon <raimon49@hotmail.com>
2019-06-28 21:41:10 +0900

    Fix spell
    Genarated
       ^

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Genarated command by : code --list-extensions | xargs -L 1 echo code --install-extension
+# Generated command by : code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension ryanolsonx.zenburn
 code --install-extension tickleforce.scrolloff
 code --install-extension vscodevim.vim
