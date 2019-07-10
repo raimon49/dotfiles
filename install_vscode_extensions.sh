ec531d401f34a9887c98bca7bc2a94131160131e
raimon <raimon49@hotmail.com>
2019-07-11 00:28:47 +0900

    Add an extension as 'TODO Highlight' for VS Code

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-# Generated command by : code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension ryanolsonx.zenburn
 code --install-extension tickleforce.scrolloff
 code --install-extension vscodevim.vim
+code --install-extension wayou.vscode-todo-highlight
