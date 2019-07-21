34be477e2581975892b3d0c535566f28698170ba
raimon <raimon49@hotmail.com>
2019-07-21 18:18:34 +0900

    Set '--force' option in VS Code extension installation script

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -2,7 +2,7 @@
 
 set -e
 set -x
-code --install-extension ryanolsonx.zenburn
-code --install-extension tickleforce.scrolloff
-code --install-extension vscodevim.vim
-code --install-extension wayou.vscode-todo-highlight
+code --install-extension ryanolsonx.zenburn --force
+code --install-extension tickleforce.scrolloff --force
+code --install-extension vscodevim.vim --force
+code --install-extension wayou.vscode-todo-highlight --force
