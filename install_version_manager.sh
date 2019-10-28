432cee2692f36318417ad2b525b8a70d6807af1c
raimon <raimon49@hotmail.com>
2019-10-29 00:09:29 +0900

    Stop the dependence on tilde expansion

--- install_version_manager.sh
+++ install_version_manager.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # anyenv
-[ ! -d "${HOME}/.anyenv" ] && \
+
+ANYENV_PATH="${HOME}/anyenv"
+[ ! -d "${ANYENV_PATH}" ] && \
     git clone https://github.com/riywo/anyenv ~/.anyenv
-    mkdir "${HOME}/.anyenv/plugins"
-    git clone https://github.com/znz/anyenv-git.git "${HOME}/.anyenv/plugins/anyenv-git"
-    git clone https://github.com/znz/anyenv-update.git "${HOME}/.anyenv/plugins/anyenv-update"
+    mkdir "${ANYENV_PATH}/plugins"
+    git clone https://github.com/znz/anyenv-git.git "${ANYENV_PATH}/plugins/anyenv-git"
+    git clone https://github.com/znz/anyenv-update.git "${ANYENV_PATH}/plugins/anyenv-update"
