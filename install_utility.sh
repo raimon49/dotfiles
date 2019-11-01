650cef91b0cf70bcdfa44b408fe8dbfa09fd7a43
raimon <raimon49@hotmail.com>
2019-11-02 00:03:40 +0900

    Fix duplicated path

--- install_utility.sh
+++ install_utility.sh
@@ -4,4 +4,4 @@
 GIBO_PATH="${HOME}/local/bin/gibo"
 [ ! -f "${GIBO_PATH}" ] && \
     curl -L https://raw.github.com/simonwhitaker/gibo/master/gibo \
-        -o "${GIBO_PATH}" && chmod +x "${GIBO_PATH}" && "${GIBO_PATH}gibo" update
+        -o "${GIBO_PATH}" && chmod +x "${GIBO_PATH}" && "${GIBO_PATH}" update
