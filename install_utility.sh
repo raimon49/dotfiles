db57f3e765fbd15bf3b61edd0551535d467a20f6
raimon <raimon49@hotmail.com>
2019-11-02 00:00:07 +0900

    Fix command of gibo at after installed

--- install_utility.sh
+++ install_utility.sh
@@ -4,4 +4,4 @@
 GIBO_PATH="${HOME}/local/bin/gibo"
 [ ! -f "${GIBO_PATH}" ] && \
     curl -L https://raw.github.com/simonwhitaker/gibo/master/gibo \
-        -o "${GIBO_PATH}" && chmod +x "${GIBO_PATH}" && gibo -u
+        -o "${GIBO_PATH}" && chmod +x "${GIBO_PATH}" && "${GIBO_PATH}gibo" update
