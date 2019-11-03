236b32151fda8c1eb71ffb750485defddf0cdb04
raimon <raimon49@hotmail.com>
2019-11-04 00:42:53 +0900

    Migrate test script from row shell command to file

---
+++ tests/test-make-directories.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+[ -d "${HOME}/local/bin" ] && \
+[ -d "${HOME}/.zsh/cache" ] && \
+[ -d "${HOME}/.config/nvim" ] && \
+[ -d "${HOME}/.config/git" ] && \
+[ -d "${HOME}/.config/Code/User" ] && \
+echo "OK: directory"
