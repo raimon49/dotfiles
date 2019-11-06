6684859c6f15cc57cd93214b2c7faae7227f56fb
raimon <raimon49@hotmail.com>
2019-11-06 23:40:57 +0900

    Fix style of line continuation

--- tests/test-xdg-base-directory.sh
+++ tests/test-xdg-base-directory.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-[ -e "${HOME}/.config/nvim" ] && \\
-[ -e "${HOME}/.config/nvim/init.vim" ] && \\
-[ -e "${HOME}/.config/git/ignore" ] && \\
-[ -e "${HOME}/.config/Code/User/settings.json" ] && \\
-[ -e "${HOME}/.config/Code/User/keybindings.json" ] && \\
+[ -e "${HOME}/.config/nvim" ] && \
+[ -e "${HOME}/.config/nvim/init.vim" ] && \
+[ -e "${HOME}/.config/git/ignore" ] && \
+[ -e "${HOME}/.config/Code/User/settings.json" ] && \
+[ -e "${HOME}/.config/Code/User/keybindings.json" ] && \
 echo "OK: XDG Base Directory"
