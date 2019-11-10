6e2a7f307dc5c6e3a19b3a86df774ab9e38342c9
raimon <raimon49@hotmail.com>
2019-11-10 23:06:30 +0900

    Test files of Bash

--- tests/test-dot-symlinks.sh
+++ tests/test-dot-symlinks.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 [ -e "${HOME}/.gitconfig" ] && \
 grep "name = raimon" "${HOME}/.gitconfig" && \
+[ -e "${HOME}/.bash_profile" ] && \
+[ -e "${HOME}/.bashrc" ] && \
+grep "HISTFILESIZE=1000000" "${HOME}/.bashrc" && \
 echo "OK: dot symlinks"
