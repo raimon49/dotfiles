4c33d8c1b423469a4078c803aad13c341657bbd8
raimon <raimon49@hotmail.com>
2019-11-13 00:10:33 +0900

    Test self build binary path in my PATH

--- tests/test-dot-symlinks.sh
+++ tests/test-dot-symlinks.sh
@@ -7,4 +7,6 @@ grep "HISTFILESIZE=1000000" "${HOME}/.bashrc" && \
 [ -e "${HOME}/.zshenv" ] && \
 [ -e "${HOME}/.zshrc" ] && \
 grep "SAVEHIST=1000000" "${HOME}/.zshrc" && \
+[ -e "${HOME}/.path.sh" ] && \
+echo ${PATH} | grep "${HOME}/local/bin:" && \
 echo "OK: dot symlinks"
