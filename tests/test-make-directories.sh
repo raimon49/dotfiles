42fff7c28c2a16c35ad112e2be54e123c670bb95
raimon <raimon49@hotmail.com>
2020-01-26 21:30:34 +0900

    Fix mistake for NeoVim

--- tests/test-make-directories.sh
+++ tests/test-make-directories.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 [ -d "${HOME}/local/bin" ] && \
 [ -d "${HOME}/.zsh/cache" ] && \
-[ -d "${HOME}/.config/nvim" ] && \
 [ -d "${HOME}/.config/git" ] && \
 [ -d "${HOME}/.config/Code/User" ] && \
 echo "OK: make directories"
