1047cbe0b42b7a1e53168349ae2310d770bc592b
raimon <raimon49@hotmail.com>
2020-05-26 21:28:12 +0900

    Add plugins for my development life cycle

--- install_asdf_plugins.sh
+++ install_asdf_plugins.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+asdf plugin add cf
+asdf plugin add conftest
 asdf plugin add golang
+asdf plugin add k9s
 asdf plugin add kubectl
 asdf plugin add nodejs
 asdf plugin add python
 asdf plugin add ruby
 asdf plugin add rust
+asdf plugin add shellcheck
