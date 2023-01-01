08ae8e5eb022648d84f6d683a0725d0df7539e60
raimon <raimon49@hotmail.com>
2023-01-01 10:48:03 +0900

    Update my UserScript template

--- .vim/template/javascript/base-my-userscript.js
+++ .vim/template/javascript/base-my-userscript.js
@@ -5,6 +5,8 @@
 // @match
 // @exclude
 // @grant       none
+// @downloadURL https://github.com/raimon49/userscripts/raw/master/{{_name_}}.user.js
+// @updateURL   https://github.com/raimon49/userscripts/raw/master/{{_name_}}.user.js
 // @author      raimon
 // @version     1.0.0
 // ==/UserScript==
